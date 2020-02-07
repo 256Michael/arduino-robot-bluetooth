acea2742f6ecc7bfee90f85b479aa27460449f95
256Michael <256Michal@gmail.com>
2020-02-07 15:35:59 +0100

    Add indicators and emargency lights

--- arduino-robot-bluetooth.ino
+++ arduino-robot-bluetooth.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#define LEFTINDICATOR 2
+#define RIGHTINDICATOR 3
+#define INDICATORSDELAY 300 
 
+char indicators = 'n';
+
+void setup() {
+  pinMode(LEFTINDICATOR, OUTPUT);
+  pinMode(RIGHTINDICATOR, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if(Serial.available()){
+    char data = Serial.read();
+    switch(data)
+    {
+      case 'l':
+        indicators = 'l';
+        break;
+      case 'r':
+        indicators = 'r';
+        break;
+      case 'e':
+        indicators = 'e';
+        break;
+      case 'n':
+        indicators = 'n';
+        break;
+    }
+  }
+  delay(INDICATORSDELAY);
+  switch(indicators){
+    case 'l':
+      digitalWrite(LEFTINDICATOR, HIGH);
+      delay(INDICATORSDELAY);
+      digitalWrite(LEFTINDICATOR, LOW);
+      break;
+    case 'r':
+      digitalWrite(RIGHTINDICATOR, HIGH);
+      delay(INDICATORSDELAY);
+      digitalWrite(RIGHTINDICATOR, LOW);
+      break;
+    case 'e':
+      digitalWrite(LEFTINDICATOR, HIGH);
+      digitalWrite(RIGHTINDICATOR, HIGH);
+      delay(INDICATORSDELAY);
+      digitalWrite(LEFTINDICATOR, LOW);
+      digitalWrite(RIGHTINDICATOR, LOW);
+      break;
+  }
 }
